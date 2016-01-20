4d1c50ad61c16db907db047759cba5aa74c73d22
proroka <amanda.prorok@gmail.com>
2016-01-20 13:27:05 -0500

    added plots

--- analytical/plot_data.py
+++ analytical/plot_data.py
@@ -6,7 +6,8 @@ import pickle
 import matplotlib.colors as colors
 
 
-run = 5
+# good runs: 2,5,6,7,8
+run = 8
 
 save_epsilon = 'data/epsilon_data_run' + str(run) + '.p'
 save_params = 'data/params_run' + str(run) + '.p'
@@ -43,14 +44,64 @@ fig = plt.figure(figsize=(5,4))
 
 dx = (np.max(par_range) - np.min(par_range)) / (2. * float(len(par_range) - 1))
 if run==2:
-    plt.imshow(data,cmap=cmap,interpolation='nearest', origin='lower',
-    extent=[np.min(par_range) - dx, np.max(par_range) + dx, np.min(par_range) - dx,
-    np.max(par_range) + dx],norm=colors.PowerNorm(gamma=2.5), clim=(2.,11.))
+    #plt.imshow(data,cmap=cmap,interpolation='nearest', origin='lower',
+    #extent=[np.min(par_range) - dx, np.max(par_range) + dx, np.min(par_range) - dx,
+    #np.max(par_range) + dx],norm=colors.PowerNorm(gamma=2.5), clim=(2.,11.))
+    clim = (2.,11.)    
+    norm = colors.PowerNorm(gamma=2.5)    
+    title = 'a+ = a- = b+ = b- = 1.0, N_3 = 200'
+    xlabel ='N_2'
+    ylabel = 'N_1'
 else:
-    plt.imshow(data,cmap=cmap,interpolation='nearest', origin='lower')#,
-#        extent=[np.min(par_range) - dx, np.max(par_range) + dx, np.min(par_range) - dx,
-#        np.max(par_range) + dx])
-
+    if run==5:
+        extent=[np.min(par_range)-dx, np.max(par_range)+dx, np.min(par_range)-dx,
+        np.max(par_range)+dx]
+        clim = (1.,9.)    
+        norm = colors.PowerNorm(gamma=0.5)  
+        #norm = None
+        title = 'N_1 = N_2 = 220, N_3 = 200'
+        xlabel = 'b-'
+        ylabel = 'a-'
+    if run==6:
+        extent=[np.min(par_range)-dx, np.max(par_range)+dx, np.min(par_range)-dx,
+        np.max(par_range)+dx]
+        clim = (1.,9.)    
+        norm = colors.PowerNorm(gamma=0.5) 
+        #norm = None
+        title = 'N_1 = N_2 = 220, N_3 = 200'
+        xlabel = 'b+'
+        ylabel = 'a+'
+    if run==7:
+        extent=[np.min(par_range)-dx, np.max(par_range)+dx, np.min(par_range)-dx,
+        np.max(par_range)+dx]
+        clim = (1.,9.)    
+        norm = colors.PowerNorm(gamma=0.5)  
+        #norm = None
+        title = 'N_1 = N_2 = 220, N_3 = 200'
+        xlabel = 'b-'
+        ylabel = 'a+'
+    if run==8:
+        extent=[np.min(par_range)-dx, np.max(par_range)+dx, np.min(par_range)-dx,
+        np.max(par_range)+dx]
+        clim = (1.,9.)    
+        norm = colors.PowerNorm(gamma=0.5)  
+        norm = None
+        title = 'N_1 = N_2 = 220, N_3 = 200'
+        xlabel = 'b+'
+        ylabel = 'a-'
+        
+plt.imshow(data,cmap=cmap,interpolation='nearest', origin='lower',
+        extent=extent, clim=clim, norm=norm)
+plt.title(title)
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)        
 plt.tight_layout()
 plt.colorbar()
 plt.show()
+
+figname = 'plots/aggregation_run_' + str(run) + '' + '.eps'
+fig.savefig(figname) 
+
+
+
+
